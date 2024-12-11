2f72244b26f4eee50fc6d0e2b22cdb806a8034ad
yhian6 <ychiroquera@ucvvirtual.edu.pe>
2024-12-10 19:57:02 -0500

    implementacion de búsqueda por los campos productid, suplierid y por ambos

--- backend/product_service/src/main/java/ucv/app_inventory/adapters/repositories/ProductSupplierRepository.java
+++ backend/product_service/src/main/java/ucv/app_inventory/adapters/repositories/ProductSupplierRepository.java
@@ -11,9 +11,13 @@ public interface ProductSupplierRepository extends JpaRepository<ProductSupplier
     // Obtener todas las relaciones por ID del producto
     List<ProductSupplier> findByProductId(Long productId);
 
+    // Obtener todas las relaciones por ID del proveedor
+    List<ProductSupplier> findBySupplierId(Long supplierId);
+
     // Verificar si existe una relación entre un producto y un proveedor
     boolean existsByProductIdAndSupplierId(Long productId, Long supplierId);
 
     // Buscar una relación por ID del producto y proveedor
     ProductSupplier findByProductIdAndSupplierId(Long productId, Long supplierId);
+
 }
