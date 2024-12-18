9c1ef829eb5dbec66b08899c342dbaf03355da1f
defaused <josue-correa10@hotmail.com>
2024-12-18 15:01:55 -0500

    Actualización de UserMapper

--- backend/authentication_service/src/main/java/ucv/app_inventory/login/application/UserMapper.java
+++ backend/authentication_service/src/main/java/ucv/app_inventory/login/application/UserMapper.java
@@ -1,7 +1,9 @@
 package ucv.app_inventory.login.application;
 
 import ucv.app_inventory.login.adapters.controller.dto.UserDto;
+import ucv.app_inventory.login.domain.model.MaritalStatus;
 import ucv.app_inventory.login.domain.model.Role;
+import ucv.app_inventory.login.domain.model.Sex;
 import ucv.app_inventory.login.domain.model.User;
 
 import java.util.Set;
@@ -19,6 +21,13 @@ public class UserMapper {
         userDto.setEmail(user.getEmail());
         userDto.setFirstName(user.getUserProfile().getFirstName());
         userDto.setLastName(user.getUserProfile().getLastName());
+        userDto.setDni(user.getUserProfile().getDni());
+        userDto.setAge(user.getUserProfile().getAge());
+        userDto.setBirthDate(user.getUserProfile().getBirthDate());
+        userDto.setAddress(user.getUserProfile().getAddress());
+        userDto.setPhone(user.getUserProfile().getPhone());
+        userDto.setSex(String.valueOf(user.getUserProfile().getSex()));
+        userDto.setMaritalStatus(String.valueOf(user.getUserProfile().getMaritalStatus()));
         userDto.setRoles(extractRoleNames(user.getRoles()));
         userDto.setStatus(user.getStatus().toString());
         return userDto;
@@ -33,6 +42,13 @@ public class UserMapper {
         if (user.getUserProfile() != null) {
             user.getUserProfile().setFirstName(userDto.getFirstName());
             user.getUserProfile().setLastName(userDto.getLastName());
+            user.getUserProfile().setDni(userDto.getDni());
+            user.getUserProfile().setAge(userDto.getAge());
+            user.getUserProfile().setBirthDate(userDto.getBirthDate());
+            user.getUserProfile().setAddress(userDto.getAddress());
+            user.getUserProfile().setPhone(userDto.getPhone());
+            user.getUserProfile().setSex(Sex.valueOf(userDto.getSex()));
+            user.getUserProfile().setMaritalStatus(MaritalStatus.valueOf(userDto.getMaritalStatus()));
         }
         return user;
     }
